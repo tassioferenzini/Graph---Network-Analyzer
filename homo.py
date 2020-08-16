ca905851fd371e745be3b61a25a894c681ffcc17
Tassio Ferenzini Martins Sirqueira <tassio@tassio.eti.br>
2020-08-15 21:16:22 -0300

    Para jessica
    Tese

--- homo.py
+++ homo.py
@@ -4,40 +4,42 @@ import networkx as nx
 import os
 import csv
 import codecs
-import random
 
 def main():
 
     instance_path = "/Users/tassio/PycharmProjects/Graph---Network-Analyzer"
 
     file_list = [f for f in os.listdir(instance_path)
-    if f.startswith('') and f.endswith('.csv')]
+    if f.startswith('') and f.endswith('1408.csv')]
     for filename in sorted(file_list):
-        G = nx.Graph()
+        G = nx.MultiGraph()
         print(filename)
         path = os.path.join(instance_path, filename)
         imports=0
+        calcamento = []
         with codecs.open(path,'r') as f:
+            calcamento.append("yellow")
             csv_f = csv.reader((line.replace('\0','') for line in f), delimiter=';')
             print("Creating Graph")
             for row in enumerate(csv_f):
                 if imports != 0:
-                    cep = row[1][1]
-                    pessoa = row[1][0]
-                    G.add_node(cep)
-                    G.add_node(pessoa)
-                    G.add_edge(cep, pessoa)
+                    cal = row[1][1]
+                    nis = row[1][0]
+                    cep = row[1][2]
+                    G.add_edge(cep, nis)
+                    if cal == "total":
+                        calcamento.append("red")
+                    elif cal == "nao_existe":
+                        calcamento.append("blue")
+                    else:
+                        calcamento.append("green")
 
                 if imports % 10000 == 0:
                     print("Lines Imports: {} ".format(imports))
 
                 imports+=1
 
-        C=nx.connected_component_subgraphs(G)
-        for g in C:
-            c=[random.random()]*nx.number_of_nodes(g)
-            nx.draw_random(g,node_size=40,node_color=c,vmin=0.0,vmax=1.0,with_labels=False)
-        plt.savefig('graph.png', dpi=300)
+        nx.draw(G, node_color=calcamento, with_labels=True)
         plt.show()
 
 if __name__ == '__main__':
